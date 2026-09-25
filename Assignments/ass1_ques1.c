55e0a0e8007a4a079eeaba44104d105d8b6281d4
Heena <heena26csu094@ncuindia.edu>
2026-09-25 06:52:07 +0530

    Added assignment1 ques1

---
+++ Assignments/ass1_ques1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main (void){
+
+    int age= 18 , percentage=90;
+    char ID [8]="26CSU094";
+    char grade='A';
+    
+    printf("--------------\n");
+    printf("student ID : %s\n", ID);
+    printf("Age : %d\n", age);
+    printf("Percentage : %d\n", percentage);
+    printf("Grade : %c\n", grade);
+    printf("--------------\n");
+    
+    return 0;
+}
+
+    
+
+
+
